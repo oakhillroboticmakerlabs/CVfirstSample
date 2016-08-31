81f874da3788a79011d89c52e282323b9c1cc9c3
kenmac <ken.macpherson@philips.com>
2016-08-30 20:20:54 -0400

    added samples rover, python test code

---
+++ ImageTesting.py
@@ -0,0 +1,9 @@
+
+
+Python Example 
+
+# Variables
+
+_url = 'https://api.projectoxford.ai/vision/v1/analyses'
+_key = ef8e933d53764fc7bc9a9eac9f381923 #Here you have to paste your primary key
+_maxNumRetries = 10
